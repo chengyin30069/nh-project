f9713ddd348657e5c319f809061bdf264033767e
Linus <chengyin30069@gmail.com>
2024-10-14 17:44:16 +0800

    Update input error detection

--- nh2.sh
+++ nh2.sh
@@ -26,6 +26,8 @@ curl -s https://nhentai.net/g/"$1"/ > tmp.html
 
 if [ "$(grep -o -E "404 - Not Found" tmp.html)" == "404 - Not Found" ]; then
 	echo "Gallery does not exists"
+	cd
+	rm -r "$1"
 	exit 1
 fi
 
