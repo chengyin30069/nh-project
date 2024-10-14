02a907184e16b6656e3762a6e597da67cc7fd1dd
Linus <chengyin30069@gmail.com>
2024-10-14 16:31:34 +0800

    Shellchecked

--- folder2cbz.sh
+++ folder2cbz.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 find ~/nh -type f -name "*.[0-9]*" -delete
-cd ~/nh
+cd ~/nh || exit 1
 ls | grep -E "^[0-9]*$" > tmp.txt
 while read -r line
 do
-	zip -rm $line.zip $line  #zipping the folder, only $line.zip remains
-	mv $line.zip $line.cbz   #changing file format
+	zip -rm "$line".zip "$line"  #zipping the folder, only $line.zip remains
+	mv "$line".zip "$line".cbz   #changing file format
 done < tmp.txt
 rm tmp.txt
